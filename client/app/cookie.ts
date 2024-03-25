61fb064da52e314a3ad8bb05d773f8c227e94639
VishavSingla <vishav9933@gmail.com>
2024-03-25 23:49:46 +0530

    removed some not needed files

--- client/app/cookie.ts
+++
@@ -1,9 +0,0 @@
-
-// import { cookies } from "next/headers";
-// import sessionToken from "./sessionToken";
-
-// export const cookieStore = cookies();
-
-// cookieStore.set("sessionToken",cookieStore.get("sessionToken"), { secure: true });
-
-// // console.log(cookieStore);
