ea0d304aeb56a217ca1f069c09620f2af387a89d
VishavSingla <vishav9933@gmail.com>
2024-04-13 20:54:52 +0530

    socket moved to sepatate file and message backend started

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,57 +1,30 @@
-import express, { Express, Request, Response } from "express";
+// index.ts
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import AuthRouter from "./routers/AuthRouter";
 import cors from "cors";
-import { Server } from "socket.io";
 import http from "http";
-import {ServerResponse, createServer} from "http";
-const port = process.env.PORT || 5001;
+import { initializeSocketIO } from "./socket";
 
 dotenv.config();
-
 const app: Express = express();
 const httpServer = http.createServer(app);
+const port = process.env.PORT || 5001;
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-io.on("connection",(socket)=>{
-  console.log("User Connected", socket.id);
-
-  socket.on("event:message",(data)=>{
-    console.log(data);
-    io.emit("message", JSON.stringify(data));
-  })
-
-  socket.on("disconnect",()=>{
-    console.log("User Disconnected", socket.id);
-  })
-})
+initializeSocketIO(httpServer);
 
 app.use(express.json());
-
-const corsOptions ={
-  origin:['http://localhost:3000'], 
-  credentials:true,         
-  optionSuccessStatus:200
-}
-
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
+app.use("/auth/user", AuthRouter);
 
-app.use('/auth/user',AuthRouter);
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express ts Server");
+app.get("/", (req, res) => {
+  res.send("Express TS Server");
 });
-
-// app.listen(port, () => {
-//   console.log(`[server]: Server is running at http://localhost:${port}`);
-// });
 
 httpServer.listen(port, () => {
   console.log(`Server is running on ${port}`);
